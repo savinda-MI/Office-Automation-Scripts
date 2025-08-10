2a33dbdebe43feebc9354110b5ebff7584cb9946
savinda-MI <savinda.missaka@gmail.com>
2025-08-10 08:21:06 +0530

    Copies today’s files from backup folders

---
+++ daily updated filecopy.bat
@@ -0,0 +1,51 @@
+robocopy "Z:\Olax\Olax Automated Backup\Ambatenna" "Z:\Olax\New folder\Ambatenna" /s /maxage:1		
+robocopy "Z:\Olax\Olax Automated Backup\clyde" "Z:\Olax\New folder\clyde" /s /maxage:1 				
+robocopy "Z:\Olax\Olax Automated Backup\culloden" "Z:\Olax\New folder\culloden" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\delgoda" "Z:\Olax\New folder\delgoda" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\doloswella" "Z:\Olax\New folder\doloswella" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\Head Office 12.30pm" "Z:\Olax\New folder\Head Office 12.30pm" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\frotoft" "Z:\Olax\New folder\frotoft" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\kiribathgala" "Z:\Olax\New folder\kiribathgala" /s /maxage:1 				
+robocopy "Z:\Olax\Olax Automated Backup\kiriwanaketiya" "Z:\Olax\New folder\kiriwanaketiya" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\Labookellie" "Z:\Olax\New folder\Labookellie" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\mohamedi" "Z:\Olax\New folder\mohamedi" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\niriella" "Z:\Olax\New folder\niriella" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\noragalla" "Z:\Olax\New folder\noragalla" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\peenkande" "Z:\Olax\New folder\peenkande" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\pimbura" "Z:\Olax\New folder\pimbura" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\PPL - AYR" "Z:\Olax\New folder\PPL - AYR" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\PPL - Durampitiya" "Z:\Olax\New folder\PPL - Durampitiya" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\PPL - HALPE" "Z:\Olax\New folder\PPL - HALPE" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\watapotha" "Z:\Olax\New folder\watapotha" /s /maxage:1				
+robocopy "Z:\Olax\Olax Automated Backup\weddemulla" "Z:\Olax\New folder\weddemulla" /s /maxage:1 				
+robocopy "Z:\Olax\Olax Automated Backup\PPL - Salawa "Z:\Olax\New folder\PPL - Salawa" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\PPL HO" "Z:\Olax\New folder\PPL HO" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup\PPL_Eheliyagoda" "Z:\Olax\New folder\PPL_Eheliyagoda" /s /maxage:1
+robocopy "Z:\Olax\Olax Automated Backup" "Z:\Olax\New folder" /s /maxage:1 
+				
+
+  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	 
+	  
+	  
+	   
+
+
+				
+				
+				
+				
+				
